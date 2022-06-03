073c97b507e5cf2aa93e4c5d29fc25c1f5322d45
DmityFilchev <filchev_dmitry@mail.ru>
2022-06-03 10:06:51 +0300

    'ok'

--- Task16_2/Task16_2.cpp
+++ Task16_2/Task16_2.cpp
@@ -1,5 +1,17 @@
 ﻿#include <iostream>
+#include<string>
+#include<sstream>
 int main()
-{
-    
+{   
+    std::stringstream number;
+    std::string num1;
+    std::string num2;
+    std::cout << "Enter the integer part of the number : ";
+    std::cin >> num1;
+    std::cout << "Enter the fractional part of the number : ";
+    std::cin >> num2;
+    number << num1 << "." << num2;
+    double a = std::stod(number.str());
+    std::cout <<"The resulting number " << a << std::endl;
+
 }
